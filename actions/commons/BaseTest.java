7153915e2fb38fb1d5505cc6d9a4412dc09e4804
TugAh <tunganh97tuaf@gmail.com>
2024-10-28 13:23:33 +0700

    Nopcommerce_Framework_Create

---
+++ actions/commons/BaseTest.java
@@ -0,0 +1,90 @@
+package commons;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Random;
+
+public class BaseTest {
+    WebDriver driver;
+    protected WebDriver getBrowserDriver(String browserName, String url){
+        BrowserList browser = BrowserList.valueOf(browserName.toUpperCase());
+        if (browser == BrowserList.FIREFOX){
+            driver = new FirefoxDriver();
+        } else if (browser == BrowserList.CHROME){
+            driver = (WebDriver) new ChromeDriver();
+        } else if (browser == BrowserList.EDGE) {
+            driver = (WebDriver) new EdgeDriver();
+        } else {
+            throw new RuntimeException("Browser name is not valid");
+        }
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        /* setSize để test reponsive cũng được
+        driver.manage().window().setPosition(new Point(0,0));
+        driver.manage().window().setSize(new Dimension(720,720));
+        */
+        driver.manage().window().maximize();
+        driver.get(url);
+        return driver;
+    }
+
+    protected void closeBrowser(){
+        String cmd = null;
+        try {
+            String osName = GlobalConstant.OS_NAME.toLowerCase();
+
+            String driverInstanceName = driver.toString().toLowerCase();
+
+            String browserDriverName = null;
+
+            if (driverInstanceName.contains("chrome")) {
+                browserDriverName = "chromedriver";
+            } else if (driverInstanceName.contains("internetexplorer")) {
+                browserDriverName = "IEDriverServer";
+            } else if (driverInstanceName.contains("firefox")) {
+                browserDriverName = "geckodriver";
+            } else if (driverInstanceName.contains("edge")) {
+                browserDriverName = "msedgedriver";
+            } else if (driverInstanceName.contains("opera")) {
+                browserDriverName = "operadriver";
+            } else {
+                browserDriverName = "safaridriver";
+            }
+
+            if (osName.contains("window")) {
+                cmd = "taskkill /F /FI \"IMAGENAME eq " + browserDriverName + "*\"";
+            } else {
+                cmd = "pkill " + browserDriverName;
+            }
+            // close browser
+            if (driver != null) {
+                driver.manage().deleteAllCookies();
+                driver.quit();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // quit driver
+            try {
+                Process process = Runtime.getRuntime().exec(cmd);
+                process.waitFor();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    protected String getRandom(){
+        Random rand = new Random();
+        int x = rand.nextInt(1,999999);
+        return Integer.toString(x);
+    }
+
+}
---
+++ actions/commons/GlobalConstant.java
@@ -0,0 +1,14 @@
+package commons;
+
+import java.io.File;
+
+public class GlobalConstant {
+    public static final long LONG_TIMEOUT = 30;
+    public static final long SHORT_TIMEOUT = 15;
+
+    public static final String ADMIN_ID = "automationtunganh";
+    public static final String ADMIN_PASSWORD = "Automationtunganh123@";
+    public static final String RELATIVE_PATH = System.getProperty("user.dir");
+    public static final String UPLOAD_FILE_PATH = RELATIVE_PATH + File.separator + "uploadFiles" + File.separator;
+    public static final String OS_NAME = System.getProperty("os.name");
+}
